364e7018a5c9661e315b318bb633f2087f98c859
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-06-08 11:37:11 -0300

    Reemplazadas las imagenes remotas por locales Excepto en el Footer que usará un placeholder en caso de error

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,23 @@
+/*
+ * Configura el comportamiento del menú y el resaltado de elementos activos al
+ * pasar el mouse sobre ellos.
+ */
 const menu = document.getElementById("navbar");
-
 Array.from(document.getElementsByClassName("menu-item")).forEach((item, index) => {
     item.onmouseover = () => {
         menu.dataset.activeIndex = index;
     };
+});
+
+
+/*
+ * Agrega un controlador de eventos para las imágenes del footer.
+ * Si una imagen falla al cargarse, se asigna una imagen de respaldo.
+ */
+document.addEventListener("DOMContentLoaded", function () {
+    document.querySelectorAll("footer img").forEach(function (img) {
+        img.addEventListener("error", function () {
+            img.src = "/SGG-Web/src/img/img_error_placeholder.png";
+        });
+    });
 });
