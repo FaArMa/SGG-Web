1ae284d84deb1b77bda85419ee942547224c3e69
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-06-17 21:03:21 -0300

    Index: se borra el comentario sobre Galeria Parallax

--- index.php
+++ index.php
@@ -36,7 +36,6 @@ session_start();
         ?>
         <h1 class="neon" data-text="U">SG<span class="flicker-slow">G</span>-<span class="flicker-fast">W</span>EB</h1>
         <p class="description">Una herramienta útil para la gestión de empleados y ventas de tu aplicación de Sistema de Gestión Gastronómico, proporcionando acceso seguro y controlado para dueños y encargados de su emprendimiento.</p>
-        <!--AGREGAR GALERIA CON PARALLAX DE IMAGENES GENERICAS DE COCINA/GASTRONOMIA RANDOM CON PHP?-->
         <p class="description">Si no sos el dueño o encargado del emprendimiento y estás viendo este sitio web, significa que alguien configuró mal todo.</p>
     </section>
     <!-- Footer -->
