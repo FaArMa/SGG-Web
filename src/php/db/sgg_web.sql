1274f7fbe21ccf2c2bd5450660823f8406c15cf6
Fabricio <101414179+FaArMa@users.noreply.github.com>
2023-06-21 01:44:21 -0300

    Database: Nuevos datos de ejemplo para usuario Ademas de pedido_proveedor y proveedor

--- src/php/db/sgg_web.sql
+++ src/php/db/sgg_web.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Jun 17, 2023 at 09:55 PM
+-- Generation Time: Jun 21, 2023 at 06:00 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,7 +42,7 @@ CREATE TABLE `factura` (
 --
 /*
 INSERT INTO `factura` (`id_factura`, `fecha_emision`, `mesa`, `importe`, `id_usuario`) VALUES
-(1, '2022-11-26', 'D3', 5820.00, 17),
+(1, '2022-11-26', 'D3', 5820.00, 13),
 (2, '2023-06-18', 'S1', 7420.00, 10),
 (3, '2023-06-16', 'S3', 5200.00, 11),
 (4, '2023-06-16', 'S3', 14513.00, 10);
@@ -171,6 +171,20 @@ CREATE TABLE `pedido_proveedor` (
   `baja` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
+--
+-- Dumping data for table `pedido_proveedor`
+--
+/*
+INSERT INTO `pedido_proveedor` (`id_pedido`, `fecha_pedido`, `id_usuario`, `id_proveedor`, `baja`) VALUES
+(1, '2023-06-01', 3, 1, 1),
+(2, '2023-06-06', 6, 2, 1),
+(3, '2023-06-10', 4, 3, 0),
+(4, '2023-06-12', 5, 4, 0),
+(5, '2023-06-14', 3, 5, 0),
+(6, '2023-06-17', 4, 6, 1),
+(7, '2023-06-19', 5, 7, 0),
+(8, '2023-06-21', 6, 8, 0);
+*/
 -- --------------------------------------------------------
 
 --
@@ -214,6 +228,20 @@ CREATE TABLE `proveedor` (
   `baja` tinyint(1) NOT NULL DEFAULT 0
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_spanish_ci;
 
+--
+-- Dumping data for table `proveedor`
+--
+/*
+INSERT INTO `proveedor` (`id_proveedor`, `nombre`, `mail`, `descripcion`, `baja`) VALUES
+(1, 'Distribuidora La Vega', 'info@lavega.com', 'Proveedor de frutas y verduras frescas.', 0),
+(2, 'Carnicería San José', 'ventas@carniceriasanjose.com', 'Especialistas en carnes de alta calidad.', 0),
+(3, 'Distribuciones Marítimas', 'info@distribucionesmaritimas.com', 'Proveedor de pescados y mariscos frescos.', 0),
+(4, 'Quesos Artesanos', 'ventas@quesosartesanos.com', 'Amplia variedad de quesos artesanales.', 0),
+(5, 'Panadería La Delicia', 'contacto@panaderialadelicia.com', 'Elaboración propia de panes y productos de reposte', 0),
+(6, 'Bebidas del Mundo', 'info@bebidasdelmundo.com', 'Proveedor de bebidas alcohólicas y no alcohólicas.', 0),
+(7, 'Distribuidora de Vinos Finos', 'ventas@vinosfinos.com', 'Amplia selección de vinos nacionales e importados.', 0),
+(8, 'Dulces y Golosinas S.A.', 'info@dulcesygolosinas.com', 'Proveedor de dulces y golosinas de todo tipo.', 0);
+*/
 -- --------------------------------------------------------
 
 --
@@ -236,28 +264,22 @@ CREATE TABLE `usuario` (
 --
 /*
 INSERT INTO `usuario` (`id_usuario`, `nombre`, `apellido`, `dni`, `rol`, `nombre_usuario`, `contrasena`, `baja`) VALUES
-(1, 'Franco', 'Armani', 37865891, 0, 'FArmaniX', 'cc68de04c617222341c717b39aca9e6c', 0),
-(2, 'Rogelio', 'Funes Mori', 38912551, 2, 'RFunesMori', 'd80d91676c77dbb1cbf9efd9bfd19368', 1),
-(3, 'Lucas', 'Beltran', 40105829, 3, 'LBeltran', '3008d81b43a8123d7b21795d04ac9884', 1),
-(4, 'Marcelo', 'Gallardo', 10100100, 1, 'mgallardo', '7dd220d2f7426185e4e394475bbfde2a', 0),
-(5, 'Enzo', 'Perez', 12345678, 0, 'eperez', '674f3c2c1a8a6f90461e8a66fb5550ba', 0),
-(6, 'Pablo', 'Almodovar', 14096861, 2, 'palmodovar', 'a957aa96221d0b7cb99ab072eaed7bf9', 1),
-(7, 'jose martin', 'campanella', 25978432, 3, 'jcampanella', '117ffc1acd844e431a4b73f0867adae5', 1),
-(8, 'martin', 'demichelis', 13246587, 0, 'mdemichelis', '2e2c4bf7ceaa4712a72dd5ee136dc9a8', 0),
-(9, 'milton', 'casco', 34086581, 1, 'mcasco', '7aee5d5dfa97b2516e5f639672c7e199', 0),
-(10, 'marcelo', 'barovero', 32194532, 2, 'mbarovero', '1d38dd921e15520709f86320185c5e1d', 0),
-(11, 'lucas', 'alario', 42654910, 2, 'lalario', '43975bc2dfc84641a2a8c4d3fe653176', 0),
-(12, 'jonatan', 'maidana', 12345679, 2, 'jmaidana', '166cee72e93a992007a89b39eb29628b', 0),
-(13, 'Ezequiel', 'Palacios', 13456789, 1, 'epalacios', '46d045ff5190f6ea93739da6c0aa19bc', 0),
-(14, 'tabare', 'viudez', 31465876, 1, 'tviudez', 'fdc0eb412a84fa549afe68373d9087e9', 0),
-(15, 'manuel', 'lanzini', 12346666, 3, 'mlanzini', 'e9510081ac30ffa83f10b68cde1cac07', 0),
-(16, 'lucas', 'pratto', 45000111, 2, 'lpratto', '7d7c45b9a935cf9d845fc75679a41559', 0),
-(17, 'Sebastián', 'Driussi', 11222333, 2, 'sdriussi', 'f7e0b956540676a129760a3eae309294', 0),
-(18, 'javier', 'pinola', 33322211, 0, 'jpinola', 'f7e0b956540676a129760a3eae309294', 0),
-(19, 'pepe', 'argento', 11122233, 0, 'pargento', 'e0f7a4d0ef9b84b83b693bbf3feb8e6e', 1),
-(20, 'esequiel', 'barco', 22111333, 0, 'ebarco', 'ff49cc40a8890e6a60f40ff3026d2730', 0),
-(21, 'ramon', 'diaz', 99888222, 3, 'rdiaz', '532923f11ac97d3e7cb0130315b067dc', 0),
-(22, 'pedo', 'feo', 11222999, 2, 'pfeo', 'a36e841c5230a79c2102036d2e259848', 1);
+(1, 'María', 'López', 28345967, 0, 'mlopez', '$argon2id$v=19$m=65536,t=4,p=1$bElXUzhpRDFXeC50VWZJRg$DBUwc+v/9YwTDbiZ5cgURevTpIyLcMy2xGC9QvQT73Q', 0),
+(2, 'Juan', 'Martínez', 76592341, 0, 'jmartinez', '$argon2id$v=19$m=65536,t=4,p=1$TFphUWtUd2QwVTZGd2s4dg$QKFMyI9VhCW8Ef0DJhBDfAAfxe2or2N17mTK6/vooGw', 0),
+(3, 'Ana', 'García', 43871235, 1, 'agarcia', '$argon2id$v=19$m=65536,t=4,p=1$SkEwQS8yak11VUtFZDVXTw$m/CS46hdcFnX8spH835vv4DVHmx5Mm7TDm0UPbxCrxs', 0),
+(4, 'Luis', 'Rodríguez', 62987453, 1, 'lrodriguez', '$argon2id$v=19$m=65536,t=4,p=1$SUFzRWN3bXdTckQ4ZlFLSQ$hTqsLlMx/x/pOJi2BILYx6PHIP2Hno4tdaD4lQGdYIw', 1),
+(5, 'Laura', 'Fernández', 18453627, 1, 'lfernandez', '$argon2id$v=19$m=65536,t=4,p=1$T0VzZW0zUVJJYlc5clV5dw$gmr5D92FlYYJiFoHWkPLcbHdMtN0kLRA+dUIkY4gOy8', 0),
+(6, 'Andrés', 'Sánchez', 37198245, 1, 'asanchez', '$argon2id$v=19$m=65536,t=4,p=1$SG9Yd1VlZmgyN0Rjc3F4aA$qXGGBcBG7VzTXLc6XFPlOhhB1v8NJDSuycvUgCgfog0', 0),
+(7, 'Carolina', 'González', 89674532, 2, 'cgonzalez', '$argon2id$v=19$m=65536,t=4,p=1$SkZlR3prQURWeDVzckFyQg$A4sVXnGKdiCKgvbmwYtTK+9evzlAukftfpS1KunZQyM', 0),
+(8, 'Pedro', 'Ramírez', 51973468, 2, 'pramirez', '$argon2id$v=19$m=65536,t=4,p=1$TnQudjNSZTk1M2pzcC5rbg$3VVhKo8r4KwKATs1ie7lSQF0DYL2bFhE9a8o6qe6Pzw', 0),
+(9, 'Sofía', 'Torres', 28764513, 2, 'storres', '$argon2id$v=19$m=65536,t=4,p=1$L2p0S2Rvb2lsdWlzRDFWWQ$EFzu+vYFmi5+/rdcbKxK0sLbIOIL5xiUK42b8ZxFqWI', 0),
+(10, 'Gabriel', 'Herrera', 65347821, 2, 'gherrera', '$argon2id$v=19$m=65536,t=4,p=1$a3lpcEZCNGFVTzNkU2M0Mg$ck0lv2TMvyxuR065/I9YSmJpVzQEPmIaSnh2PxPY1tM', 0),
+(11, 'Valentina', 'Castro', 14256983, 2, 'vcastro', '$argon2id$v=19$m=65536,t=4,p=1$RjNFWUNaekE2bk5JcTZFeA$+imhJRdG7m9eziiavtTQEYNhvmk52qOzXBAyesfLQes', 0),
+(12, 'Alejandro', 'Jiménez', 72519384, 2, 'ajimenez', '$argon2id$v=19$m=65536,t=4,p=1$NmxyOXdRYVRsMlFYNlZ1dg$1B+eGz6Z8sEOmodK8fTYpvfaDIlrn3E4GNhAmLQodO0', 0),
+(13, 'Camila', 'Ríos', 38465792, 2, 'crios', '$argon2id$v=19$m=65536,t=4,p=1$WmpITVVqa2tzUGt5UXdVRQ$lhM+BIekcxRuuvDGjqxDYSibEqPVftmZIG2hFdpXaOE', 1),
+(14, 'Martín', 'Silva', 91625478, 2, 'msilva', '$argon2id$v=19$m=65536,t=4,p=1$RFY4cVlwM0VkVEN4Vk9TVQ$NLVJnmDdzBQKYqqJ0HID715PHFIP1KRMX4mwxTshziY', 1),
+(15, 'Victoria', 'Ortiz', 58234697, 3, 'vortiz', '$argon2id$v=19$m=65536,t=4,p=1$VkNoMGFsdWF3RGVaVnZNQw$zJSE/ipMznJ7XQ5EysNmQlDnUvR09jojB9SoExBpu98', 1),
+(16, 'Manuel', 'Paredes', 49762183, 3, 'mparedes', '$argon2id$v=19$m=65536,t=4,p=1$aC9OWWljaUZnV25IN25JUg$r+ihqxvzzggSH5OpwDl+897Hs0Gkm36yVyrJ4pI0hv4', 0);
 */
 --
 -- Indexes for dumped tables
